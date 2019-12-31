b8fc5c1de44e33a8723b6220c4d36a62f877865c
Amalie <amalih@stud.ntnu.no>
2019-12-31 18:14:01 +0100

    Update PF_DQN.py

--- Lcontainer/PF_DQN.py
+++ Lcontainer/PF_DQN.py
@@ -29,7 +29,7 @@ REPLAY_MEMORY_SIZE = 1_000_000  # How many last steps to keep for model training
 MIN_REPLAY_MEMORY_SIZE = 264 #inimum number of steps in a memory to start training
 MINIBATCH_SIZE = 264 # How many steps (samples) to use for training
 UPDATE_TARGET_EVERY = 1  # Terminal states (end of episodes)
-MODEL_NAME = 'LContainer-Eps08_03_02_01-Epochs4-Eps200_1000_2000_2000-Steps1500-linear-CNN_300_200_100-YEMAX800-LR001-Outputs29-UpdateTarget1-Triangle20-MB264'
+MODEL_NAME = 'LContainer-Eps08_03_02_01-Epochs4-Eps200_1000_2000_2000-Steps1500-linear-CNN_400_300-YEMAX800-LR001-Outputs29-UpdateTarget1-Triangle20-MB264'
 MIN_REWARD = 0  # For model save
 OBSERVATION_SPACE_VALUES = 6
 ACTION_SPACE_VALUES = 29
@@ -115,10 +115,10 @@ class DQN_Agent:
 
         model = Sequential()
 
-        model.add(Dense(300, input_shape=(1,OBSERVATION_SPACE_VALUES), activation='relu'))
+        model.add(Dense(400, input_shape=(1,OBSERVATION_SPACE_VALUES), activation='relu'))
         #model.add(BatchNormalization())
-        model.add(Dense(200, activation='relu'))
-        model.add(Dense(100, activation='relu'))
+        model.add(Dense(300, activation='relu'))
+        #model.add(Dense(100, activation='relu'))
         model.add(Flatten())
         model.add(Dense(ACTION_SPACE_VALUES, activation='linear'))
 
